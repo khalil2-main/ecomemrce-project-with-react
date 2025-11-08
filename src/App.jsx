e31c3a3c9faac70519b9fb641286673ae88bd59d
Circe-melki <midory798464@gmail.com>
2025-11-08 18:53:34 +0100

    reorganize pages in more clear and organize richtecure

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 
-import { HomePage } from './pages/homePage'
+import { HomePage } from './pages/homepage/HomePage'
+import { Routes } from 'react-router'
+import { Route } from 'react-router'
 import './App.css'
+import { CheckoutPage } from './pages/checkout/CheckoutPage'
+import { OrdersPage } from './pages/Orders/OrdersPage'
 
 function App() {
  
 
   return (
    <>
-   <HomePage></HomePage>
+   <Routes>
+    <Route path='' element={<HomePage/>}/>
+    <Route path='checkout' element={ <CheckoutPage/>}/>
+    <Route path='orders' element={ <OrdersPage/>}/>
+   </Routes>
+
     </>
   )
 }
