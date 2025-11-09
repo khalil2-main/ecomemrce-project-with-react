589219af4af23e92d0a9c3d4051d731980de9bf5
Circe-melki <midory798464@gmail.com>
2025-11-09 09:15:35 +0100

    add api data

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -1,14 +1,30 @@
-import { Header } from '../../component/header/header'
 
-import{ products }from '../../../starting-code/data/products.js'
+import axios from 'axios'
+
 import './homePage.css'
-import { ProductContainer } from '../../component/header/product-container/PoductContainer'
+import { ProductContainer } from '../../components/header/product-container/PoductContainer'
+import { Header } from '../../components/header/header.jsx'
+import { useEffect, useState } from 'react'
 
 export function HomePage(){
+ const [products,setProducts]= useState([]);
+  const [cart,setCart]=useState([]);
+  useEffect(()=>{
+    axios.get('http://localhost:3000/api/products').then((res)=>{
+      setProducts(res.data);
+  
+    })   
+  },[])
+  //cart items
+  useEffect(()=>{
+    axios.get('http://localhost:3000/api/cart-items').then((res=>{
+      setCart(res.data);
+    }))
+  },[])
   return<>
   <title>Ecomorce project</title>
    
-    <Header></Header>
+    <Header cart={cart} />
     <div className="home-page">
       <div className="products-grid">
        { products.map((product)=>{
